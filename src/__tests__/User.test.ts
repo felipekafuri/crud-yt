f2e1791702789a94a6480724d8db7c2270eea2d1
felipekafuri <felipekafuri@discente.ufg.br>
2021-03-12 10:00:53 -0300

    tests

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import { getConnection } from 'typeorm'
+import { app } from '../app'
+import createConnection from '../shared/typeorm'
+
+describe('User', () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+
+    await connection.runMigrations()
+  })
+
+  afterAll(async () => {
+    const connection = getConnection()
+
+    await connection.dropDatabase()
+
+    await connection.close()
+  })
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@gmail.com',
+      password: '123456'
+    })
+
+    expect(response.status).toBe(201)
+    expect(response.body).toHaveProperty('id')
+  })
+})
